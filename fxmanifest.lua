03fe0e6b16da453b9ff50638212fd3de493fb286
senganull <ayanokoji.roito@gmail.com>
2025-06-21 16:43:13 +0900

    create ui. files

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,6 +4,7 @@ game 'gta5'
 author 'Null'
 description 'script template for FiveM'
 version ' 1.0.0'
+lua54 'yes'
 
 client_scripts {
     'client.lua'
@@ -15,4 +16,13 @@ server_scripts {
 
 shared_scripts {
     'config.lua'
+}
+
+ui_page 'ui/index.html'
+
+files {
+    'ui/index.html',
+    'ui/style.css',
+    'ui/script.js',
+    'ui/img/*.png',
 }
