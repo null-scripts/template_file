1a97c5ef8965085934c2e9d9115910910c1b3038
senganull <ayanokoji.roito@gmail.com>
2025-06-21 16:34:57 +0900

    fix fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Null'
+description 'tutorial command'
+version ' 1.0.0'
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
+
+shared_scripts {
+    'config.lua'
+}
